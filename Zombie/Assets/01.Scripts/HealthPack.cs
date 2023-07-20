308600d021639c6befa658a34fdc3bbd9b305c1c
강동윤 <davidkang8111@gmail.com>
2023-07-20 12:34:25 +0900

    20230720
    20230720

--- Zombie/Assets/01.Scripts/HealthPack.cs
+++ Zombie/Assets/01.Scripts/HealthPack.cs
@@ -2,23 +2,21 @@ using UnityEngine;
 
 public class HealthPack : MonoBehaviour, IItem
 {
-    public float health = 50f;
+    public float health = 50f; // 체력을 회복할 수치
 
     public void Use(GameObject target)
     {
-        // target의 체력을 회복하는 처리
-        Debug.Log("체력을 회복했다 : " + health);
-    }
+        // 전달받은 게임 오브젝트로부터 LivingEntity 컴포넌트 가져오기 시도
+        LivingEntity life = target.GetComponent<LivingEntity>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        // LivingEntity 컴포넌트가 있다면
+        if(life != null)
+        {
+            // 체력 회복 실행
+            life.RestoreHealth(health);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 사용되었으므로 자신을 파괴
+        Destroy(this.gameObject);
     }
 }
